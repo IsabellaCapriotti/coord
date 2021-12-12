a03853398949dfab19064123a5a3b8a931f21561
Isabella <isabellamcapriotti@gmail.com>
2021-12-12 14:24:58 -0800

    Added ability to move images in editor

--- coord/src/editor-img/editor-img.component.ts
+++ coord/src/editor-img/editor-img.component.ts
@@ -10,28 +10,42 @@ export class EditorImgComponent implements AfterViewInit{
     @Input() imageSrc: string = ""; 
     @ViewChild('imgContainer') imgContainer !: ElementRef; 
 
-    
+    // Resizing 
     currHeight : number = -1; 
     currWidth : number = -1; 
-
     isDraggingResizer : boolean = false; 
     resizeStartX : number = 0; 
     resizeStartY : number = 0; 
     resizeStartWidth : number = 0; 
     resizeStartHeight : number = 0; 
 
+    // Moving 
+    currLeftOffset : number = 0; 
+    currTopOffset : number = 0; 
+    isMoving : boolean = false; 
+    moveStartX : number = 0; 
+    moveStartY : number = 0; 
+    moveStartLeftOffset : number = 0; 
+    moveStartTopOffset : number = 0; 
+
     ngAfterViewInit(): void {
-        console.log(this.imgContainer); 
+        console.log(this.imgContainer.nativeElement.getBoundingClientRect()); 
 
         // Get initial height and width of image
         this.currHeight = this.imgContainer.nativeElement.offsetHeight; 
-        this.currWidth = this.imgContainer.nativeElement.offsetWidth; 
+        this.currWidth = this.imgContainer.nativeElement.offsetWidth;
     }
 
+
+    // **********************************************
+    // RESIZING
+    // **********************************************
     onResizerMousedown(event:any){
-        console.log('clicked ' + event.clientX + ' ' + event.clientY);
-        
+    
+        // Start dragging process
         this.isDraggingResizer = true; 
+
+        // Save initial values of mouse position and element dimensions
         this.resizeStartX = event.clientX; 
         this.resizeStartY = event.clientY; 
         this.resizeStartHeight = this.imgContainer.nativeElement.offsetHeight; 
@@ -39,35 +53,68 @@ export class EditorImgComponent implements AfterViewInit{
     }
 
     @HostListener('window:mousemove', ['$event'])
-    onResizerMousemove(event:any){
+    onMousemove(event:any){
 
+        // Handle resize 
         if(this.isDraggingResizer){
-            //console.log('dragging'); 
 
             // Add distance dragged to original dimensions 
             let widthDiff = event.clientX - this.resizeStartX; 
             let heightDiff = event.clientY - this.resizeStartY; 
-
      
             this.currWidth = this.resizeStartWidth + widthDiff; 
             this.currHeight = this.resizeStartHeight + heightDiff; 
             
-          
-            console.log(this.currWidth); 
-            console.log(this.currHeight)
- 
+        }
+
+        // Handle move
+        if(this.isMoving){
+            
+            // Add to offset based on distance dragged
+            let xDiff = event.clientX - this.moveStartX; 
+            let yDiff = event.clientY - this.moveStartY; 
+
+            this.currLeftOffset = this.moveStartLeftOffset + xDiff; 
+            this.currTopOffset = this.moveStartTopOffset + yDiff; 
+
+            // Don't allow image out of bounds
+            if(this.currLeftOffset < 0){
+                this.currLeftOffset = 0; 
+            }
+            if(this.currTopOffset < 0){
+                this.currTopOffset = 0; 
+            }
         }
     }
 
     @HostListener('window:mouseup', ['$event'])
-    onResizerMouseup(event:any){
+    onMouseup(event:any){
 
-        console.log('stopped clicking ' + event.clientX + event.clientY); 
-
+        // End resize and move once mouse is released
         this.isDraggingResizer = false; 
+        this.isMoving = false; 
 
-       
     }
 
+    // **********************************************
+    // MOVING
+    // **********************************************
+    onMoverMousedown(event:any){
 
+        // Start moving process
+        this.isMoving = true; 
+
+        // Save initial values
+        this.moveStartLeftOffset = this.currLeftOffset; 
+        this.moveStartTopOffset = this.currTopOffset; 
+        this.moveStartX = event.clientX; 
+        this.moveStartY = event.clientY; 
+
+    }
+
+    disableDrag(event:any){
+        console.log('DRAG HERRRRRR')
+        event.preventDefault(); 
+        event.stopPropagation(); 
+    }
 }
